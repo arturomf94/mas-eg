3b6f43f2dea6b51a76d612557ace7381da548e10
arturomf94 <arturomf94@gmail.com>
2018-09-30 21:52:02 -0500

    add binary constraints

--- chapter-1/graph-coloring.py
+++ chapter-1/graph-coloring.py
@@ -1,4 +1,5 @@
 import mas
+import random
 
 #### Graph coloring problems: 
 
@@ -14,10 +15,14 @@ eg1 = mas.MAS(3, [[0,1,1],[1,0,1],[1,1,0]])
 
 domains = [['red'],['red', 'blue'], ['red', 'blue', 'green']]
 
-# Assign domains and values for each agent
+# Assign domains, values and binary constraints for each agent
 
-for i, agent in enumerate(eg1.agent_list):
-	agent.domain = domains[i]
+for agent in eg1.agent_list:
+	agent.domain = domains[agent.id_number]
 	agent.value = random.choice(agent.domain)
+	agent.binary_constraints = [(agent.id_number, other_agent.id_number) for 
+									other_agent in eg1.agent_list 
+									if other_agent.id_number in agent.neighbors]
+
 
 import pdb;pdb.set_trace()
