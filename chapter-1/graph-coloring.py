7b15736569cc0e44feb6816d20c1a463f980b676
arturomf94 <arturomf94@gmail.com>
2018-09-30 23:58:31 -0500

    fix bug that caused premature termination and add examples

--- chapter-1/graph-coloring.py
+++ chapter-1/graph-coloring.py
@@ -23,5 +23,79 @@ for agent in eg1.agent_list:
 	agent.binary_constraints = [(agent.id_number, other_agent.id_number) for 
 									other_agent in agent.neighbors]
 
-
+print('\n')
+print('EXAMPLE 1')
 eg1.run_arc_consistency()
+
+### Example 2:
+
+## Setup: 
+
+# Instantiate MAS with adjecency matrix
+
+eg2 = mas.MAS(3, [[0,1,1],[1,0,1],[1,1,0]])
+
+# Declare domains
+
+domains = [['red'],['red', 'blue'], ['red', 'blue']]
+
+# Assign domains, values and binary constraints for each agent
+
+for agent in eg2.agent_list:
+	agent.domain = domains[agent.id_number]
+	agent.value = random.choice(agent.domain)
+	agent.binary_constraints = [(agent.id_number, other_agent.id_number) for 
+									other_agent in agent.neighbors]
+
+print('\n')
+print('EXAMPLE 2')
+eg2.run_arc_consistency()
+
+
+### Example 3:
+
+## Setup: 
+
+# Instantiate MAS with adjecency matrix
+
+eg3 = mas.MAS(3, [[0,1,1],[1,0,1],[1,1,0]])
+
+# Declare domains
+
+domains = [['red', 'blue'],['red', 'blue'], ['red', 'blue']]
+
+# Assign domains, values and binary constraints for each agent
+
+for agent in eg3.agent_list:
+	agent.domain = domains[agent.id_number]
+	agent.value = random.choice(agent.domain)
+	agent.binary_constraints = [(agent.id_number, other_agent.id_number) for 
+									other_agent in agent.neighbors]
+
+print('\n')
+print('EXAMPLE 3')
+eg3.run_arc_consistency()
+
+### Example 4:
+
+## Setup: 
+
+# Instantiate MAS with adjecency matrix
+
+eg4 = mas.MAS(3, [[0,1,1],[1,0,1],[1,1,0]])
+
+# Declare domains
+
+domains = [['red', 'blue', 'green'],['red', 'blue', 'green'], ['red', 'blue', 'green']]
+
+# Assign domains, values and binary constraints for each agent
+
+for agent in eg4.agent_list:
+	agent.domain = domains[agent.id_number]
+	agent.value = random.choice(agent.domain)
+	agent.binary_constraints = [(agent.id_number, other_agent.id_number) for 
+									other_agent in agent.neighbors]
+
+print('\n')
+print('EXAMPLE 4')
+eg4.run_arc_consistency()
--- chapter-1/mas.py
+++ chapter-1/mas.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 class MAS:
     def __init__(self, nb_agents, adjacency_matrix):
@@ -26,19 +27,30 @@ class MAS:
 
     def run_arc_consistency(self):
         finished = False
+        final_tick = sys.maxint
         while not finished:
             self.tick += 1
-            agents_finished = []
+            agents_no_solution_flag = []
+            agents_domain_lengths = []
             for agent in self.agent_list:
-                agents_finished.append(agent.finished_flag)
+                agents_no_solution_flag.append(agent.no_solution_flag)
+                agents_domain_lengths.append(len(agent.domain))
+            if self.tick == final_tick or True in agents_no_solution_flag:
+                finished = True
+                if self.tick == final_tick:
+                    print('Solution found!')
+                if True in agents_no_solution_flag:
+                    print('There is no solution!')
+                break
             for agent in self.agent_list:
                 print('Agent ' + str(agent.id_number) + ' has domain: \n')
                 print(agent.domain)
                 agent.arc_consistency()
-            if not False in agents_finished:
-                finished = True
+            if all(length == 1 for length in agents_domain_lengths):
+                final_tick = self.tick + 1
             print("tick " + str(self.tick) + " ended")
             if self.tick == 5:
+                print('Could not find solution!')
                 break
        
 class Agent:
@@ -49,7 +61,7 @@ class Agent:
         self.value = ''
         self.binary_constraints = []
         self.messages = []
-        self.finished_flag = False
+        self.no_solution_flag = False
             
     def decide(self, tick):
         print('something')
@@ -66,8 +78,8 @@ class Agent:
                 if (self.id_number, message[0]) in self.binary_constraints and \
                     message[1] in self.domain:
                     self.domain.remove(message[1])
-        if len(self.domain) == 1 or len(self.domain) == 0:
-            self.finished_flag = True
+        if len(self.domain) == 0:
+            self.no_solution_flag = True
 
     # def revise(self):
     #     for message in self.messages:
